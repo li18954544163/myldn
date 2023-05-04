ccb46b1f4a38135c8acd7f8ba9e23887a212fc69
李东娜 <12272359+im-li-dongna@user.noreply.gitee.com>
2023-05-04 15:30:56 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>lv</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+
+  data () {
+    return {
+
+      info: '我我我我是是是是刘刘刘刘梦梦梦梦！！！！！！'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
